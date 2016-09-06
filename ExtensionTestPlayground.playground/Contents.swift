580f934027730baee39a1ffcd69a5e22416411c4
Ellen Shapiro <designatednerd@gmail.com>
2016-09-06 11:25:39 +0100

    Add playground

---
+++ ExtensionTestPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//The flavor of this color
+//allows the behavior to center
+//before the beginning of the program.
+let color = UIColor.red
