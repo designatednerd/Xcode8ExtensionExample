9b7358633b90cec49753f7b93541dd51e73f7e3f
Ellen Shapiro <designatednerd@gmail.com>
2016-10-14 03:11:10 +0200

    Add FlavorKit

---
+++ FlavorFlavour/FlavorKit/FlavorKit.h
@@ -0,0 +1,20 @@
+//
+//  FlavorKit.h
+//  FlavorKit
+//
+//  Created by Ellen Shapiro (Work) on 10/14/16.
+//  Copyright © 2016 Designated Nerd Software. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FlavorKit.
+FOUNDATION_EXPORT double FlavorKitVersionNumber;
+
+//! Project version string for FlavorKit.
+FOUNDATION_EXPORT const unsigned char FlavorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlavorKit/PublicHeader.h>
+#import <FlavorKit/CFNotificationCenterWrapper.h>
+
+
